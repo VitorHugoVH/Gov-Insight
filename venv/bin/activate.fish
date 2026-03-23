9f4dc909085408c127b1ff701781a1c7604e07b2
1arthurncc <nichelearthur@gmail.com>
2026-03-23 18:05:40 -0300

    002

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/vitorhugo/gov_insight/venv
+set -gx VIRTUAL_ENV /home/arthur/Gov-Insight/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
